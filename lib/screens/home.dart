b266d0586187f9cf4cbbcd930610ceaa18db28f6
paraweethongnoi <parawee191@gmail.com>
2021-02-25 16:03:46 +0700

    ล่าสุด 2502

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,55 +1,55 @@
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-// import 'package:tess/screens/bmi1.dart';
-// import 'package:tess/screens/register.dart';
-// import 'package:tess/screens/signin.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:tess/screens/bmi1.dart';
+import 'package:tess/screens/register.dart';
+import 'package:tess/screens/signin.dart';
 
-// class Home extends StatefulWidget {
-//   @override
-//   _HomeState createState() => _HomeState();
-// }
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
 
-// class _HomeState extends State<Home> {
-//   //Method
+class _HomeState extends State<Home> {
+  //Method
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     checkStatus();
-//   }
+  @override
+  void initState() {
+    super.initState();
+    checkStatus();
+  }
 
-//   Future<void> checkStatus() async {
-//     FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
-//     final user = await _firebaseAuth.currentUser;
+  Future<void> checkStatus() async {
+    FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+    final user = await _firebaseAuth.currentUser;
 
-//     if (user != null) {
-//       MaterialPageRoute materialPageRoute =
-//           MaterialPageRoute(builder: (BuildContext context) => Bmi1());
-//       Navigator.of(context).pushAndRemoveUntil(
-//           materialPageRoute, (Route<dynamic> route) => false);
-//     }
-//   }
+    if (user != null) {
+      MaterialPageRoute materialPageRoute =
+          MaterialPageRoute(builder: (BuildContext context) => Bmi1());
+      Navigator.of(context).pushAndRemoveUntil(
+          materialPageRoute, (Route<dynamic> route) => false);
+    }
+  }
 
-//   Widget showLogo() {
-//     return Container(
-//       width: 190.0,
-//       height: 190.0,
-//       child: Image.asset('assets/images/logo.png'),
-//     );
-//   }
+  Widget showLogo() {
+    return Container(
+      width: 190.0,
+      height: 190.0,
+      child: Image.asset('assets/images/logo.png'),
+    );
+  }
 
-//   Widget showAppName() {
-//     return Text(
-//       'Thai herbs',
-//       style: TextStyle(
-//           fontSize: 45.0,
-//           color: Colors.green.shade700,
-//           fontWeight: FontWeight.bold,
-//           fontStyle: FontStyle.italic,
-//           fontFamily: 'Courgette'),
-//     );
-//   }
-//////////////////////////////////////////////////////////////////////////////
+  Widget showAppName() {
+    return Text(
+      'Thai herbs',
+      style: TextStyle(
+          fontSize: 45.0,
+          color: Colors.green.shade700,
+          fontWeight: FontWeight.bold,
+          fontStyle: FontStyle.italic,
+          fontFamily: 'Courgette'),
+    );
+  }
+////////////////////////////////////////////////////////////////////////////
   // Widget signInButton() {
   //   return Container(
   //     width: 350.0,
@@ -107,36 +107,36 @@
   //       SizedBox(
   //         width: 50.0,
   //       ),
-  //      // signUpButton(),
+  //      //signUpButton(),
   //     ],
   //   );
   // }
-////////////////////////////////////////////////////////////////////////////////////
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         body: SafeArea(
-//       child: Container(
-//         decoration: BoxDecoration(
-//           gradient: RadialGradient(
-//             colors: [Colors.yellow.shade100, Colors.green],
-//             radius: 2.0,
-//           ),
-//         ),
-//         child: Center(
-//           child: Column(
-//             mainAxisSize: MainAxisSize.min,
-//             children: <Widget>[
-//               showLogo(),
-//               showAppName(),
-//               SizedBox(
-//                 height: 40.0,
-//               ),
-//               //showButton(),
-//             ],
-//           ),
-//         ),
-//       ),
-//     ));
-//   }
-// }
+//////////////////////////////////////////////////////////////////////////////////
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            colors: [Colors.yellow.shade100, Colors.green],
+            radius: 2.0,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              showLogo(),
+              showAppName(),
+              SizedBox(
+                height: 40.0,
+              ),
+             // showButton(),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
